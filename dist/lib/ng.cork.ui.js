a17098668b326fcae5ab120ecbc096b7ca8372ff
Andre Torgal <andre.torgal@gmail.com>
2015-04-09 03:08:06 +0100

    chore: dist

--- dist/lib/ng.cork.ui.js
+++ dist/lib/ng.cork.ui.js
@@ -2,9 +2,11 @@
     'use strict';
 
     var module = angular.module('ng.cork.ui', [
+        'ng.cork.ui.keys',
         'ng.cork.ui.enter-click',
         'ng.cork.ui.focus-on',
-        'ng.cork.ui.stop-propagation'
+        'ng.cork.ui.stop-propagation',
+        'ng.cork.ui.textarea-auto-resize'
     ]);
 
 })(angular);
--- dist/ng.cork.ui.js
+++ dist/ng.cork.ui.js
@@ -1,5 +1,5 @@
 /**
- * ng.cork.ui - v0.0.4 - 2015-04-08
+ * ng.cork.ui - v0.0.4 - 2015-04-09
  * https://github.com/cork-labs/ng.cork.ui
  *
  * Copyright (c) 2015 Cork Labs <http://cork-labs.org>
@@ -8,245 +8,221 @@
 (function (angular) {
     'use strict';
 
-    var module = angular.module('ng.cork.ui.enter-click', []);
-
-    /**
-     * @ngdoc directive
-     * @name ng.cork.ui.enter-click.corkUiEnterClick
-     *
-     * @description
-     * When element is focused, converts "Enter" keypress events into click events.
-     *
-     * If element is not focusable, i.e., does not have a `tabindex` attribute, then `tabindex` is forced to `0`.
-     *
-     * @restrict A
-     */
-    module.directive('corkUiEnterClick', [
-        '$document',
-        function ($document) {
-            return {
-                restrict: 'A',
-                link: function ($scope, $element, $attrs) {
-                    if (!$attrs.tabindex) {
-                        $element.attr('tabindex', 0);
-                    }
-                    $element.on('keypress', function (ev) {
-                        if (ev.keyCode === 13) {
-                            $element.triggerHandler('click');
-                        }
-                    });
-                }
-            };
-        }
-    ]);
-
-})(angular);
-
-(function (angular) {
-    'use strict';
-
-    var module = angular.module('ng.cork.ui.focus-on', []);
-
-    /**
-     * @ngdoc directive
-     * @name ng.cork.ui.focus-on.corkUiFocusOn
-     *
-     * @description
-     * Focus element when a certain message is received or immediately after rendering.
-     *
-     * @restrict A
-     *
-     * @param {string} corkUiFocusOn Via attribute, not watched. Set to "focus" to focus automaticaaly on render, set to a string to listen for that message name.
-     */
-    module.directive('corkUiFocusOn', [
-        '$timeout',
-        function ($timeout) {
-            return function ($scope, $element, $attrs) {
-                var corkUiFocusOn = $attrs.corkUiFocusOn;
-                if (!$attrs.tabindex) {
-                    $element.attr('tabindex', 0);
-                }
-
-                function setFocus() {
-                    $timeout(function () {
-                        $element[0].focus();
-                    });
-                }
-                if (corkUiFocusOn === 'auto') {
-                    setFocus();
-                } else {
-                    $scope.$on(corkUiFocusOn, setFocus);
-                }
-            };
-        }
-    ]);
-
-})(angular);
-
-(function (angular) {
-    'use strict';
-
-    var module = angular.module('ng.cork.ui.stop-propagation', []);
-
-    /**
-     * @ngdoc directive
-     * @name ng.cork.ui.stop-propagation.corkUiStopPropagation
-     *
-     * @description
-     * Prevents propagation of certain events through this element.
-     *
-     * @scope
-     * @restrict A
-     *
-     * @param {boolean=} corkUiStopPropagation Provide an expression. When it evaluates to TRUE, propagation will not be stopped.
-     * @param {array=} events Optional, list of event names to prevent Defaults to `['click']`
-     */
-    module.directive('corkUiStopPropagation', [
-        '$timeout',
-        function ($timeout) {
-            return {
-                restrict: 'A',
-                scope: {
-                    disabled: '=corkUiStopPropagation',
-                    events: '=corkUiPreventEvents'
-                },
-                link: function ($scope, $element, $attrs) {
-
-                    /**
-                     * @var {object} listeners currently bound
-                     */
-                    var listeners = {};
-
-                    /**
-                     * event handler registered in the browser
-                     * @param {object} ev Native event.
-                     */
-                    var StopPropagation = function (ev) {
-                        if (!$scope.disabled) {
-                            ev.stopPropagation();
-                        }
-                    };
-
-                    /**
-                     * register event listeners
-                     */
-                    function updateEventListeners(events) {
-                        var eventName;
-                        var ix;
-                        // remove listeners that are no longer in the event list
-                        for (eventName in listeners) {
-                            if (events.indexOf(eventName) === -1) {
-                                $element[0].removeEventListener(eventName, StopPropagation);
-                            }
-                            delete listeners.eventName;
-                        }
-                        // add listeners for events not yet in the listeners map
-                        for (ix = 0; ix < events.length; ix++) {
-                            eventName = events[ix];
-                            if (!listeners[eventName]) {
-                                listeners[eventName] = $element[0].addEventListener(eventName, StopPropagation);
-                            }
-                        }
-                    }
-
-                    /**
-                     * watch $scope.events for changes, bind/unbind
-                     */
-                    $scope.$watch('events', function () {
-                        if (angular.isArray($scope.events)) {
-                            updateEventListeners($scope.events);
-                        } else {
-                            updateEventListeners(['click']);
-                        }
-                    });
-
-                    /**
-                     * unregister ALL event listeners when scope is destroyed
-                     */
-                    $scope.$on('$destroy', function () {
-                        updateEventListeners([]);
-                    });
-                }
-            };
-        }
-    ]);
-
-})(angular);
-
-(function (angular) {
-    'use strict';
-
-    var module = angular.module('ng.cork.ui.textarea-auto-resize', []);
-
-    /**
-     * @ngdoc directive
-     * @name ng.cork.ui.textarea-auto-resize.corkUiTextareaAutoResize
-     *
-     * @description
-     * Resizes textarea via `style.height` after user input.
-     *
-     * @example
-     */
-    module.directive('corkUiTextareaAutoResize', [
-        '$document',
-        function ($document) {
-            return {
-                restrict: 'A',
-                require: '?ngModel',
-                link: function ($scope, $element, $attrs, ngModelController) {
-
-                    var document = $document[0];
-                    var documentElement = document.documentElement;
-                    var input = $element[0];
-                    var style = input.style;
-
-                    style.overflow = 'hidden';
-
-                    var getHeightOffset = function () {
-                        if (style.boxSizing === 'content-box') {
-                            return -(parseFloat(style.paddingTop) + parseFloat(style.paddingBottom));
-                        } else {
-                            return parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth);
-                        }
-                    };
-
-                    var resize = function () {
-                        var htmlTop = documentElement.scrollTop;
-                        var bodyTop = document.body.scrollTop;
-                        var heightOffset = getHeightOffset();
-                        style.height = '1px';
-                        var targetHeight = input.scrollHeight + (isNaN(heightOffset) ? 0 : heightOffset);
-                        style.height = (targetHeight + 10) + 'px';
-                        documentElement.scrollTop = htmlTop;
-                        document.body.scrollTop = bodyTop;
-                    };
-
-                    input.addEventListener('keyup', resize, true);
-                    resize();
-
-                    $scope.$on('$destroy', function () {
-                        input.removeEventListener('keyup', resize, true);
-                    });
-
-                    if (ngModelController) {
-                        $scope.$watch(function () {
-                            return ngModelController.$viewValue;
-                        }, resize);
-                    }
-                }
-            };
-        }
-    ]);
-
-})(angular);
-
-(function (angular) {
-    'use strict';
-
     var module = angular.module('ng.cork.ui', [
+        'ng.cork.ui.keys',
         'ng.cork.ui.enter-click',
         'ng.cork.ui.focus-on',
-        'ng.cork.ui.stop-propagation'
+        'ng.cork.ui.stop-propagation',
+        'ng.cork.ui.textarea-auto-resize'
     ]);
 
 })(angular);
+
+! function (a) {
+    "use strict";
+    var b = a.module("ng.cork.ui.enter-click", []);
+    b.directive("corkUiEnterClick", ["$document", function (a) {
+        return {
+            restrict: "A",
+            link: function (a, b, c) {
+                c.tabindex || b.attr("tabindex", 0), b.on("keypress", function (a) {
+                    13 === a.keyCode && b.triggerHandler("click")
+                })
+            }
+        }
+    }])
+}(angular);
+
+! function (a) {
+    "use strict";
+    var b = a.module("ng.cork.ui.focus-on", []);
+    b.directive("corkUiFocusOn", ["$timeout", function (a) {
+        return function (b, c, d) {
+            function e() {
+                a(function () {
+                    c[0].focus()
+                })
+            }
+            var f = d.corkUiFocusOn;
+            d.tabindex || c.attr("tabindex", 0), "auto" === f ? e() : b.$on(f, e)
+        }
+    }])
+}(angular);
+
+! function (a) {
+    "use strict";
+    var b, c, d, e, f = a.module("ng.cork.ui.keys", ["ng.cork.deep.extend"]),
+        g = "deletion",
+        h = "navigation",
+        i = "movement",
+        j = "pagination",
+        k = "direction",
+        l = [{
+            c: 8,
+            n: "Backspace",
+            t: [g, h]
+        }, {
+            c: 9,
+            n: "Tab",
+            t: [h]
+        }, {
+            c: 13,
+            n: "Enter",
+            t: [h]
+        }, {
+            c: 27,
+            n: "Esc",
+            t: [h]
+        }, {
+            c: 33,
+            n: "PageUp",
+            t: [i, j]
+        }, {
+            c: 34,
+            n: "PageDown",
+            t: [i, j]
+        }, {
+            c: 35,
+            n: "End",
+            t: [i, j]
+        }, {
+            c: 36,
+            n: "Home",
+            t: [i, j]
+        }, {
+            c: 37,
+            n: "Left",
+            t: [i, k]
+        }, {
+            c: 38,
+            n: "Up",
+            t: [i, k]
+        }, {
+            c: 39,
+            n: "Right",
+            t: [i, k]
+        }, {
+            c: 40,
+            n: "Down",
+            t: [i, k]
+        }, {
+            c: 45,
+            n: "Insert",
+            t: []
+        }, {
+            c: 46,
+            n: "Delete",
+            t: [g]
+        }],
+        m = {
+            ie: {
+                map: {},
+                filter: function (a) {
+                    return a
+                }
+            },
+            opera: {
+                map: {
+                    5: 10
+                }
+            }
+        },
+        n = {},
+        o = {};
+    for (c = 0; c < l.length; c++)
+        if (b = l[c], n[b.n] = b.c, b.t)
+            for (d = 0; d < b.t.length; d++) e = b.t[d], o[e] || (o[e] = []), o[e].push(b.c);
+    f.constant("corkUiKeysCodes", n), f.provider("corkUiKeys", ["corkDeepExtend", "corkUiKeysCodes", function (a, b) {
+        var c = this,
+            d = {
+                tags: o
+            };
+        c.configure = function (b) {
+            a(d, b)
+        }, c.$get = [function () {
+            function a(a) {
+                "function" == typeof c.filter && (a = c.filter(a)), c.amp[a]
+            }
+            var b, c = m[b],
+                d = {
+                    key: n,
+                    getCode: function (b) {
+                        var d = b.charCode ? b.charCode : b.keyCode;
+                        return c && (d = a(d)), d
+                    },
+                    is: function (a, b) {
+                        return !!o[a] && -1 !== o[a].indexOf(b)
+                    }
+                };
+            return d
+        }]
+    }])
+}(angular);
+
+! function (a) {
+    "use strict";
+    var b = a.module("ng.cork.ui.stop-propagation", []);
+    b.directive("corkUiStopPropagation", ["$timeout", function (b) {
+        return {
+            restrict: "A",
+            scope: {
+                disabled: "=corkUiStopPropagation",
+                events: "=corkUiPreventEvents"
+            },
+            link: function (b, c, d) {
+                function e(a) {
+                    var b, d;
+                    for (b in f) - 1 === a.indexOf(b) && c[0].removeEventListener(b, g), delete f.eventName;
+                    for (d = 0; d < a.length; d++) b = a[d], f[b] || (f[b] = c[0].addEventListener(b, g))
+                }
+                var f = {},
+                    g = function (a) {
+                        b.disabled || a.stopPropagation()
+                    };
+                b.$watch("events", function () {
+                    e(a.isArray(b.events) ? b.events : ["click"])
+                }), b.$on("$destroy", function () {
+                    e([])
+                })
+            }
+        }
+    }])
+}(angular);
+
+! function (a) {
+    "use strict";
+    var b = a.module("ng.cork.ui.textarea-auto-resize", []);
+    b.directive("corkUiTextareaAutoResize", ["$document", function (a) {
+        return {
+            restrict: "A",
+            require: "?ngModel",
+            link: function (b, c, d, e) {
+                var f = a[0],
+                    g = f.documentElement,
+                    h = c[0],
+                    i = h.style;
+                i.overflow = "hidden";
+                var j = function () {
+                        return "content-box" === i.boxSizing ? -(parseFloat(i.paddingTop) + parseFloat(i.paddingBottom)) : parseFloat(i.borderTopWidth) + parseFloat(i.borderBottomWidth)
+                    },
+                    k = function () {
+                        var a = g.scrollTop,
+                            b = f.body.scrollTop,
+                            c = j();
+                        i.height = "1px";
+                        var d = h.scrollHeight + (isNaN(c) ? 0 : c);
+                        i.height = d + 10 + "px", g.scrollTop = a, f.body.scrollTop = b
+                    };
+                e && b.$watch(function () {
+                    return h.value
+                }, function () {
+                    k()
+                }), h.addEventListener("keyup", function () {
+                    k()
+                }), k(), b.$on("$destroy", function () {
+                    h.removeEventListener("keyup", k)
+                })
+            }
+        }
+    }])
+}(angular);
