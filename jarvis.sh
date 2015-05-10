0cd42c7d2f61bdeaf2728ff48afc4c4f49a131f5
Andre Torgal <andre.torgal@gmail.com>
2015-05-11 01:45:22 +0200

    chore(build): fix jarvis.sh

--- jarvis.sh
+++ jarvis.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # build tasks
-export HOME="/tmp" && npm install && bower --allow-root update && grunt build && grunt docs
+export HOME='/tmp'
+export NODE_ENV='development'
+npm install && bower --allow-root update && grunt build && grunt docs
